300beaed6ec30ebbd44612157994d19520eb898d
FlyAIBox <fly910905@sina.com>
2025-05-02 20:37:46 +0800

    【U】完善使用 LangChain 加载目录中所有文档依赖； Ubuntu-langchain-ubuntu（没有GPU）的依赖

--- 01-数据导入-DataLoading/01-简单文本读取/03-01-用LangChain加载目录中所有文档.py
+++ 01-数据导入-DataLoading/01-简单文本读取/03-01-用LangChain加载目录中所有文档.py
@@ -1,5 +1,21 @@
+# 使用 LangChain 加载目录中所有文档
+
+"""
+# 需要安装 Tesseract OCR 
+### Ubuntu 执行如下命令：
+sudo apt update
+sudo apt install tesseract-ocr -y
+"""
+import os
 from langchain_community.document_loaders import DirectoryLoader
-loader = DirectoryLoader("./data/黑神话")
+
+# 获取当前脚本文件所在的目录
+script_dir = os.path.dirname(__file__)
+print(f"获取当前脚本文件所在的目录：{script_dir}") 
+# 结合相对路径构建完整路径
+data_dir = os.path.join(script_dir, '../../90-文档-Data/黑悟空')
+
+loader = DirectoryLoader(data_dir)
 docs = loader.load()
 print(f"文档数：{len(docs)}")  # 输出文档总数
 print(docs[0])  # 输出第一个文档
